0ccda0af3ee79904b7f30caf184e071737aeffca
rahul <rahul@BOOK-EO97RBNIMQ>
2024-09-13 15:20:08 +0530

    my first commit

---
+++ com.kodbook/src/main/java/com/kodbook/services/UserService.java
@@ -0,0 +1,16 @@
+package com.kodbook.services;
+
+import org.springframework.stereotype.Service;
+
+import com.kodbook.entities.User;
+
+@Service
+public interface UserService {
+
+	boolean userExists(String username, String email);
+
+	void addUser(User user);
+
+	boolean validateUser(String username, String password);
+
+}
